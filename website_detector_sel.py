16e78144d6ac2b485944a5bd8d50d59c0efcfc90
André Muth <andre.muth@tutanota.com>
2023-12-30 21:08:21 +0100

    Adding features to website_detector_se

--- website_detector_sel.py
+++ website_detector_sel.py
@@ -38,11 +38,27 @@ for e in col_list:
         comp_col = e
         break
 
-# Start the selenium chromedriver
-driver, startpage = start_browser_sel(webdriver, Service, chromedriver_path, startpage)
-
+# Starting with an empty table
 newtable = []
+
+count = 0
+#skip = -1
+skip = 119
+
 for id, row in df_source.iterrows():
+    if id <= skip:
+        continue
+#    if id == 150:
+#        break
+    # Restart the browser after 7 search queries
+    if count == 7:
+        driver.quit()
+        count = 0
+    # Start the selenium chromedriver
+    if count == 0:
+        driver, startpage = start_browser_sel(webdriver, Service, chromedriver_path, startpage)
+    count += 1
+
     company = extract_text(row[comp_col])
     nextpos = col_list.index(comp_col) + 1
     if len(str(row[nextpos])) > 4:
@@ -53,20 +69,25 @@ for id, row in df_source.iterrows():
         key_w = company
     comp_keywords = get_company_keywords(company, row, col_list)
     search_url = search_for(driver, startpage, key_w)
-    if search_url == startpage:
-        print('Error')
-        continue
+    if search_url == startpage or 'sorry' in search_url:
+        time.sleep(7)
+    if search_url == startpage or 'sorry' in search_url:
+        search_url = search_for(driver, startpage, company)
     soup = BeautifulSoup(driver.page_source,'lxml')
     search_results = get_search_results(Comment, soup)
     website, website_options = get_website(comp_keywords, search_results)
+    if not website and len(website_options) == 0:
+        search_url = search_for(driver, startpage, company)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        search_results = get_search_results(Comment, soup)
+        website, website_options = get_website(comp_keywords, search_results)
     linklist = get_all_links(soup)
     sm_links, linklist = sm_filter(linklist)
     other_links = [l for l in linklist if not any(l in e for e in sm_links) and not any(l in e for e in website_options)]
-
     full_row = [id, company, website, website_options, sm_links, other_links]
     newtable.append(full_row)
-    if id == 5:
-        break
+    print(full_row[:3])
+    print(comp_keywords)
 
 
 # DataFrame
@@ -74,4 +95,13 @@ header = ['ID','Anbieter','Webseite','Alternative Webseiten','Social Media','Wei
 df_company_links = pd.DataFrame(newtable,columns=header)
 
 # Create an Excel file
-df_company_links.to_excel('Nahrungsergaenzungsmittel_Links.xlsx')
+dt_str_now = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+recent_filename = 'Nahrungsergaenzungsmittel_Links' + dt_str_now + '.xlsx'
+df_company_links.to_excel(recent_filename)
+
+
+newtable = newtable[:-1]
+len(newtable)
+for t in newtable:
+    print(t[:3])
+print(newtable[-1][:4])
