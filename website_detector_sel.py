5e3967ab2f88fa1d030184c93248583eea309175
André Muth <andre.muth@tutanota.com>
2024-01-02 20:39:00 +0100

    Putting everything into functions

--- website_detector_sel.py
+++ website_detector_sel.py
@@ -26,82 +26,87 @@ from datetime import datetime, timedelta
 import time
 
 ########################################################################################################################
-# Load the excel file with the required data
-# It should contain the company names and ideally some search keywords for their website
-output_path = r"C:\Users\andre\OneDrive\Desktop\Nahrungsergaenzungsmittel"
-os.chdir(output_path)
-source_file = r"Liste_Nahrungsergänzungsmittel 2024_20231227.xlsx"
-df_source = pd.read_excel(source_file)
-col_list = list(df_source.columns)
-for e in col_list:
-    if 'Firma' in e or 'Anbieter' in e:
-        comp_col = e
-        break
+def main_function(webdriver, Service, chromedriver_path, startpage, df_source, newtable, skip):
+    col_list = list(df_source.columns)
+    for e in col_list:
+        if 'Firma' in e or 'Anbieter' in e:
+            comp_col = e
+            break
+    count = 0
+    for id, row in df_source.iterrows():
+        if id <= skip:
+            continue
+    #    if id == 150:
+    #        break
+        # Restart the browser after 7 search queries
+    #    if count == 7:
+    #        driver.quit()
+    #        count = 0
+        # Start the selenium chromedriver
+        if count == 0:
+            driver, startpage = start_browser_sel(webdriver, Service, chromedriver_path, startpage)
+        count += 1
 
-# Starting with an empty table
-newtable = []
-
-count = 0
-#skip = -1
-skip = 119
-
-for id, row in df_source.iterrows():
-    if id <= skip:
-        continue
-#    if id == 150:
-#        break
-    # Restart the browser after 7 search queries
-    if count == 7:
-        driver.quit()
-        count = 0
-    # Start the selenium chromedriver
-    if count == 0:
-        driver, startpage = start_browser_sel(webdriver, Service, chromedriver_path, startpage)
-    count += 1
-
-    company = extract_text(row[comp_col])
-    nextpos = col_list.index(comp_col) + 1
-    if len(str(row[nextpos])) > 4:
-        key_w = str(row[nextpos])
-    elif len(str(row[nextpos + 1])) > 4:
-        key_w = str(row[nextpos + 1])
-    else:
-        key_w = company
-    comp_keywords = get_company_keywords(company, row, col_list)
-    search_url = search_for(driver, startpage, key_w)
-    if search_url == startpage or 'sorry' in search_url:
-        time.sleep(7)
-    if search_url == startpage or 'sorry' in search_url:
-        search_url = search_for(driver, startpage, company)
-    soup = BeautifulSoup(driver.page_source,'lxml')
-    search_results = get_search_results(Comment, soup)
-    website, website_options = get_website(comp_keywords, search_results)
-    if not website and len(website_options) == 0:
-        search_url = search_for(driver, startpage, company)
-        soup = BeautifulSoup(driver.page_source, 'lxml')
+        company = extract_text(row[comp_col])
+        nextpos = col_list.index(comp_col) + 1
+        if len(str(row[nextpos])) > 4:
+            key_w = str(row[nextpos])
+        elif len(str(row[nextpos + 1])) > 4:
+            key_w = str(row[nextpos + 1])
+        else:
+            key_w = company
+        comp_keywords = get_company_keywords(company, row, col_list)
+        search_url = search_for(driver, startpage, key_w)
+        if search_url == startpage or '/sorry' in driver.current_url:
+            driver.quit()
+            skip = id -1
+            return newtable, skip
+ #           time.sleep(7)
+        soup = BeautifulSoup(driver.page_source,'lxml')
         search_results = get_search_results(Comment, soup)
         website, website_options = get_website(comp_keywords, search_results)
-    linklist = get_all_links(soup)
-    sm_links, linklist = sm_filter(linklist)
-    other_links = [l for l in linklist if not any(l in e for e in sm_links) and not any(l in e for e in website_options)]
-    full_row = [id, company, website, website_options, sm_links, other_links]
-    newtable.append(full_row)
-    print(full_row[:3])
-    print(comp_keywords)
+        if not website and len(website_options) == 0:
+            search_url = search_for(driver, startpage, company)
+            soup = BeautifulSoup(driver.page_source, 'lxml')
+            search_results = get_search_results(Comment, soup)
+            website, website_options = get_website(comp_keywords, search_results)
+        linklist = get_all_links(soup)
+        sm_links, linklist = sm_filter(linklist)
+        other_links = [l for l in linklist if not any(l in e for e in sm_links) and not any(l in e for e in website_options)]
+        full_row = [id, company, website, website_options, sm_links, other_links]
+        newtable.append(full_row)
+        print(full_row[:3])
+        print(comp_keywords)
+        skip = id
+    return newtable, skip
+
+########################################################################################################################
+# Starting with an empty table and a number of rows you want so skip
+newtable = []
+skip = -1
+
+# Run the Crawler/ Scraper
+if __name__ == '__main__':
+    # Load the excel file with the required data
+    # It should contain the company names and ideally some search keywords for their website
+    output_path = r"C:\Users\andre\OneDrive\Desktop\Nahrungsergaenzungsmittel"
+    os.chdir(output_path)
+    source_file = r"Liste_Nahrungsergänzungsmittel 2024_20231227.xlsx"
+    df_source = pd.read_excel(source_file)
+
+    newtable, skip = main_function(webdriver, Service, chromedriver_path, startpage, df_source, newtable, skip)
+
+    # DataFrame
+    header = ['ID','Anbieter','Webseite','Alternative Webseiten','Social Media','Weitere Links']
+    df_company_links = pd.DataFrame(newtable,columns=header)
+
+    # Create an Excel file
+    dt_str_now = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+    recent_filename = 'Nahrungsergaenzungsmittel_Links' + dt_str_now + '.xlsx'
+    df_company_links.to_excel(recent_filename)
 
 
-# DataFrame
-header = ['ID','Anbieter','Webseite','Alternative Webseiten','Social Media','Weitere Links']
-df_company_links = pd.DataFrame(newtable,columns=header)
-
-# Create an Excel file
-dt_str_now = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-recent_filename = 'Nahrungsergaenzungsmittel_Links' + dt_str_now + '.xlsx'
-df_company_links.to_excel(recent_filename)
-
-
-newtable = newtable[:-1]
-len(newtable)
-for t in newtable:
-    print(t[:3])
-print(newtable[-1][:4])
+#newtable = newtable[:-1]
+#for t in newtable:
+#    print(t[:3])
+#print(newtable[-1][:4])
