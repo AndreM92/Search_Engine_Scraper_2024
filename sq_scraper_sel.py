973b4750a95867fc89933d0baf6072029de37340
André Muth <andre.muth@tutanota.com>
2024-01-19 10:25:25 +0100

    Tested version 1.0

--- sq_scraper_sel.py
+++ sq_scraper_sel.py
@@ -19,9 +19,12 @@ from datetime import datetime, timedelta
 import time
 
 import os
+chromedriver_path = r"C:\Users\andre\Documents\Python\chromedriver-win64\chromedriver.exe"
 path_to_crawler_functions = r'C:\Users\andre\Documents\Python\Web_Scraper\Search_Engine_Scraper_2024'
 file_path = r"C:\Users\andre\OneDrive\Desktop\Nahrungsergaenzungsmittel"
-chromedriver_path = r"C:\Users\andre\Documents\Python\chromedriver-win64\chromedriver.exe"
+source_file = 'Liste_Nahrungsergänzungsmittel_2024_Auswahl.xlsx'
+branch_keywords = ['nutrition', 'vitamin', 'mineral', 'protein', 'supplement', 'diet', 'health', 'ernährung',
+                   'ergänzung', 'gesundheit', 'nährstoff', 'fitness', 'sport', 'leistung']
 ########################################################################################################################
 
 def compose_search_url(platform, company):
@@ -35,25 +38,74 @@ def collect_search_results(driver):
     results = get_search_results(soup)
     return results
 
+def rank_sm_accounts(platform, comp_keywords, branch_keywords, search_results):
+    p_link = platform.lower() + '.com/'
+    not_profile = ['/post', 'hashtag', 'sharer','/status', 'photo/', 'photos', 'watch?', '/video/', 'discover', '.help',
+                    'reels', 'story', 'explore', 'playlist', 'sharer', 'policy', 'privacy', 'instagram.com/p/',
+                   '/tag/','/embed/']
+    accounts = [row for row in search_results if p_link in row[0] and any(k in row[0] for k in comp_keywords)
+                and not any(n in row[0] for n in not_profile)]
+    ranking_dict = {}
+    for pos, row in enumerate(accounts):
+        link, title, content = row
+        ranking_dict[link] = len(accounts) - pos
+        link_part = link.split(p_link)[1].split('/')[0]
+        if link_part in comp_keywords:
+            ranking_dict[link] += 2
+        for k in comp_keywords:
+            if k in title:
+                ranking_dict[link] += 1
+            if k in content:
+                ranking_dict[link] += 1
+        for k in branch_keywords:
+            if k in title.lower():
+                ranking_dict[link] += 1
+            if k in content.lower():
+                ranking_dict[link] += 1
+    ordered_dict = sorted(ranking_dict.items(), key=lambda x:x[1], reverse=True)
+    account_list = [key for key, value in ordered_dict]
+    return account_list
+
 
 if __name__ == '__main__':
+    # Choose a Social Media Platform
+    platform = 'Facebook'
     os.chdir(path_to_crawler_functions)
     from search_crawler_functions import *
     import search_crawler_credentials as cred
     os.chdir(file_path)
-    source_file = pd.read_excel('Liste_Nahrungsergänzungsmittel_2024_Auswahl.xlsx')
+    df_source = pd.read_excel(source_file)
+    col_list = list(df_source.columns)
 
-    # Insert the company and Platform you want to search for
-    company = 'Optimum Nutrition'
-    platform = 'Instagram'
+    new_table = []
+    for count, row in df_source.iterrows():
+        id = extract_every_number(row['ID'])
+        print(count, id)
+        account = str(row[platform])
+        comp_keywords, company = get_company_keywords(row, col_list)
+#        if len(account) > 10 and any(k in account for k in comp_keywords):
+        if len(account) > 10 and company.lower() in account.lower():
+            complete_row = [id, company, account, '']
+            new_table.append(complete_row)
+            continue
 
-    search_url = compose_search_url(platform, company)
-    driver, page = start_browser_sel(chromedriver_path, search_url, headless=True)
-    search_results = collect_search_results(driver)
+        search_url = compose_search_url(platform, company)
+        driver, page = start_browser_sel(chromedriver_path, search_url, headless=True)
+        search_results = collect_search_results(driver)
+        account_list = rank_sm_accounts(platform, comp_keywords, branch_keywords, search_results)
+        if len(account_list) >= 1:
+            account = account_list.pop(0)
+        new_row = [id, company, account, account_list]
+        new_table.append(new_row)
 
-    for id, r in enumerate(search_results):
-        print(r)
-        if id == 3:
-            break
+    # Dataframe
+    header = ['ID', 'Anbieter', platform, 'alt_links']
+    df_se = pd.DataFrame(new_table, columns=header)
+
+    # Create an Excel file
+    dt_str_now = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+    recent_filename = 'Search_Results_' + platform + '_' + dt_str_now + '.xlsx'
+    df_se.to_excel(recent_filename)
+
 
     driver.quit()
